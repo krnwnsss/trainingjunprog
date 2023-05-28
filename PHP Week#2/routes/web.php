619173de2dde949f197db820c1b143b7227fdfe6
krnwnsss <114396064+krnwnsss@users.noreply.github.com>
2023-05-29 02:00:37 +0700

    commit

---
+++ PHP Week#2/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::resource('employee', EmployeeController::class);
+
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
